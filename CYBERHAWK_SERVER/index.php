8d94d098cb9f752ac9192580a289baf299a10041
Agence Cybersécurité de Néosoft <152294352+NeosoftCybersecurite@users.noreply.github.com>
2023-12-15 11:55:26 +0100

    Update index.php

--- CYBERHAWK_SERVER/index.php
+++ CYBERHAWK_SERVER/index.php
@@ -11,7 +11,7 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-		<title>CyberHawk By CONIX</title>
+		<title>CyberHawk By Néosoft</title>
 	
 	
 		<link rel="stylesheet" href="./css/font-awesome.min.css"/>
